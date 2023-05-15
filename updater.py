b7d60feed28bd293cf82a55336e709782a669bc8
justmore5mins <standby-nexuses0m@icloud.com>
2023-05-15 20:52:47 +0800

    add

--- updater.py
+++ updater.py
@@ -1,8 +1,11 @@
 from os import chdir, system
+from sys import stdout
 
 chdir("/Users/AustinYu/Documents/sever")
 
 system("java -Xmx6G -jar minecraft_server.jar --nogui")
+stdout.write("\033[2J\033[H")
+stdout.flush()
 
 system("git remote add origin https://github.com/justmore5mins/sever.git")
 system("git add .")
